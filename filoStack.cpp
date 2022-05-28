79766119be9d30805e2ebe713e9298f9946c7e07
mirage <newblood17@gmail.com>
2022-05-27 20:34:30 -0500

    Curso Cpp

--- filoStack.cpp
+++ filoStack.cpp
@@ -9,12 +9,23 @@ struct Nodo
 };
 
 void push(Nodo *&stack, int data);
-
+void remove(Nodo *&stack,int &data);
 int main(){
     Nodo *pila=NULL;
 
     push(pila,5);
     push(pila,6);
+    int num;
+    while (pila!=NULL)
+    {
+        remove(pila,num);
+        if(pila!=NULL){
+            std::cout<<num<<",";
+        }else{
+            std::cout<<num<<".";
+        }
+    }
+    
 
 
     return 0;
@@ -26,4 +37,11 @@ void push(Nodo *&stack, int data){
     newNode->next=stack;
     stack=newNode;
     std::cout<<"El dato es :"<<data<<"\n";
+}
+
+void remove(Nodo *&stack,int &data){
+    Nodo *aux=stack;
+    data=aux->data;
+    stack=aux->next;
+    delete aux;
 }
